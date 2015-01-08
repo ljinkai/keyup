954ed137ded1174c4e2cb13772d79e3419576bb2
ljinkai <ljinkai@126.com>
2015-01-08 14:26:48 +0800

    add h5

---
+++ pro/h5/shake/站点列表_流量统计_网站分析_数据专家cnzz.com_files/core(3).php
@@ -0,0 +1,10 @@
+!function(){var p,q,r,a=encodeURIComponent,b="33222",c="pic",d="",e="online_v3.php",f="hzvs2.cnzz.com",g="1",h="pic",i="z",j="&#31449;&#38271;&#32479;&#35745;",k=window["_CNZZDbridge_"+b].bobject,l="http:",m="1",n=l+"//online.cnzz.com/online/"+e,o=[];o.push("id="+b),o.push("h="+f),o.push("on="+a(d)),o.push("s="+a(c)),n+="?"+o.join("&"),"0"===m&&k.callRequest([l+"//cnzz.mmstat.com/9.gif?abc=1"]),g&&(""!==d?k.createScriptIcon(n,"utf-8"):(q="z"==i?"http://www.cnzz.com/stat/website.php?web_id="+b:"http://quanjing.cnzz.com","pic"===h?(r=l+"//icon.cnzz.com/img/"+c+".gif",p="<a href='"+q+"' target=_blank title='"+j+"'><img border=0 hspace=0 vspace=0 src='"+r+"'></a>"):p="<a href='"+q+"' target=_blank title='"+j+"'>"+j+"</a>",k.createIcon([p])))}();    (function(){
+    var cnzz_s_tag = document.createElement('link');
+    cnzz_s_tag.rel = 'WebElementscache';
+    cnzz_s_tag.type = 'text/cache-manifest';
+    cnzz_s_tag.href = 'testpath'
+    cnzz_s_tag.version = '1.0';
+    var root_s = document.getElementsByTagName('head')[0];
+    root_s.appendChild(cnzz_s_tag, root_s);
+    })();
+    
